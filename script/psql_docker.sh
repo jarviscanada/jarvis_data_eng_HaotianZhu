13a24728f6cc945cc8719067f8b6601b5ac27359
haotianzhu <centos@jrvs-remote-desktop-centos7.us-east1-c.c.vivid-case-287418.internal>
2020-08-26 16:43:44 +0000

    fix echo message, adding usage; fix else syntax error

--- script/psql_docker.sh
+++ script/psql_docker.sh
@@ -16,36 +16,45 @@ then
   if [[ $(docker ps -a --filter "name=jrvs-psql" | grep jrvs-psql) ]];
   then
     echo "jrvs-psql container already exists";
+    echo "./scripts/psql_docker.sh start|stop"
     exit 1;
   else
     echo "creating jrvs-psql container with psql database ...";
+   
     if [[ -z $2 ]] || [[ -z $3 ]];
     then
-      echo "database name/password is Empty"
+      echo "database name/password is Empty;"
+      echo "./scripts/psql_docker.sh create [db_username] [db_password]"
       exit 1;
-    if
+    fi
+
     docker volume create pgdata;
     docker run --name jrvs-psql -e POSTGRES_USER=$2 -e POSTGRES_PASSWORD=$3  -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres;
-    exit $?
+    exit $?;
   fi
 fi
 
 if [[ ! $(docker ps -a --filter "name=jrvs-psql" | grep jrvs-psql) ]];
 then
-  echo "please create container first;\n>./scripts/psql_docker.sh create [db_username] [db_password]";
+  echo "please create container first;"
+  echo "./scripts/psql_docker.sh create [db_username] [db_password]";
+  exit 1;
+fi
+
 
 if [[ $1 = "start" ]];
 then
   docker container start jrvs-psql
-  exit $?
-elif [[ $2 = "stop" ]];
+  exit $?;
+elif [[ $1 = "stop" ]];
+then
   docker container stop jrvs-psql
-  exit $?
+  exit $?;
 else
-  echo "unrecognizable;\n>./scripts/psql_docker.sh start|stop|create [db_username][db_password]"
-  exit 1
-
-
+  echo "unrecognizable;"
+  echo "./scripts/psql_docker.sh start|stop|create [db_username][db_password]"
+  exit 1;
+fi
 
 
 
