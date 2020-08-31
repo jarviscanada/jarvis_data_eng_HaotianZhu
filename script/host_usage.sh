c430e287d75e69973b4f5a3afb409312952740b0
haotianzhu <zhuhaotian19970215@gmail.com>
2020-08-31 10:48:34 -0600

    init host_usage; need test

--- script/host_usage.sh
+++ script/host_usage.sh
@@ -8,10 +8,31 @@
 # # Example
 # bash scripts/host_usage.sh localhost 5432 host_agent postgres password
 
+
+
+PSQL_HOST=$1
+PSQL_PORT=$2
+DB_NAME=$3
+PSQL_USER=$4
+
+
+# checking if zero
+if [[ -z $PSQL_HOST ]] || [[ -z $PSQL_PORT ]] || [[ -z $DB_NAME ]] || [[ -z $PSQL_USER ]] || [[ -z $PGPASSWORD ]]; then
+  echo "./scripts/host_info.sh psql_host psql_port db_name psql_user psql_password";
+  exit 1;
+fi
+
+if [[ $5 ]];then
+PGPASSWORD=$5
+fi
+
+
+
+
 meminfo=$(cat /proc/meminfo)
+hostname=$(hostname -f);
 
-
-timestamp=$(vmstat -t | grep -o -e '[0-9]\{4\}\-.*' | xargs)
+=$(vmstat -t | grep -o -e '[0-9]\{4\}\-.*' | xargs)
 
 #usage
 memory_free=$(echo "$meminfo" | egrep "^MemFree:" | awk '{print $2}' | xargs)
@@ -20,5 +41,12 @@ cpu_kernel=$(vmstat -t | grep  -Po '\d+(?=\s+\d+\s+\d+\s+\d+\s+\d+\-.*)')
 disk_io=$(vmstat -d | grep  -Po '\d+(?=\s+\d+$)')
 disk_available=$(df -BM / | grep  -Po '\d+(?=M\s+\d+%)' )
 
+insert_stmt="WITH id_table AS (SELECT id FROM host_info WHERE hostname='$hostname') INSERT INTO host_info (\"$timestamp\", \"$cpu_idle\", \"$cpu_kernel\", \"$disk_io\", \"$disk_available\", \"host_id\") VALUES ('$timestamp', $cpu_idle, $cpu_kernel, $disk_io, $disk_available, (select id from idt));"
+
 # testing
-#echo "$hostname|$cpu_number|$cpu_architecture|$cpu_model|$cpu_mhz|$l2_cache|$cpu_idle|$cpu_kernel|$disk_io|$disk_available"
+#echo $insert_stmt;
+
+psql -h $PSQL_HOST -p $PSQL_PORT  -d $DB_NAME -U $PSQL_USER  -c "$insert_stmt";
+
+exit $?
+
