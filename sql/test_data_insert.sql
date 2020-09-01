32d327009c34719a3af4f39a654002e9ee4ce7a7
haotianzhu <zhuhaotian19970215@gmail.com>
2020-09-01 09:02:25 -0600

    init sql script

---
+++ sql/test_data_insert.sql
@@ -0,0 +1,22 @@
+INSERT INTO host_usage ("timestamp", host_id, memory_free, cpu_idel, cpu_kernel, disk_io,disk_available)VALUES ('2019-05-29 15:00:00.000', 1, 300000, 90, 4, 2, 3);
+INSERT INTO host_usage ("timestamp", host_id, memory_free, cpu_idel, cpu_kernel, disk_io,disk_available)VALUES ('2019-05-29 15:01:00.000', 1, 100000, 90, 4, 2, 3);
+INSERT INTO host_usage ("timestamp", host_id, memory_free, cpu_idel, cpu_kernel, disk_io,disk_available)VALUES ('2019-05-29 15:02:00.000', 1, 200000, 90, 4, 2, 3);
+INSERT INTO host_usage ("timestamp", host_id, memory_free, cpu_idel, cpu_kernel, disk_io,disk_available)VALUES ('2019-05-29 15:03:00.000', 1, 100000, 90, 4, 2, 3);
+INSERT INTO host_usage ("timestamp", host_id, memory_free, cpu_idel, cpu_kernel, disk_io,disk_available)VALUES ('2019-05-29 15:04:00.000', 1, 200000, 90, 4, 2, 3);
+INSERT INTO host_usage ("timestamp", host_id, memory_free, cpu_idel, cpu_kernel, disk_io,disk_available)VALUES ('2019-05-29 15:05:00.000', 1, 300000, 90, 4, 2, 3);
+INSERT INTO host_usage ("timestamp", host_id, memory_free, cpu_idel, cpu_kernel, disk_io,disk_available)VALUES ('2019-05-29 15:06:00.000', 1, 100000, 90, 4, 2, 3);
+INSERT INTO host_usage ("timestamp", host_id, memory_free, cpu_idel, cpu_kernel, disk_io,disk_available)VALUES ('2019-05-29 15:07:00.000', 1, 200000, 90, 4, 2, 3);
+INSERT INTO host_usage ("timestamp", host_id, memory_free, cpu_idel, cpu_kernel, disk_io,disk_available)VALUES ('2019-05-29 15:08:00.000', 1, 100000, 90, 4, 2, 3);
+INSERT INTO host_usage ("timestamp", host_id, memory_free, cpu_idel, cpu_kernel, disk_io,disk_available)VALUES ('2019-05-29 15:09:00.000', 1, 200000, 90, 4, 2, 3);
+
+
+
+INSERT INTO host_usage ("timestamp", host_id, memory_free, cpu_idel, cpu_kernel, disk_io,disk_available)VALUES ('2019-05-29 15:00:00.000', 2, 300000, 90, 4, 2, 3);
+INSERT INTO host_usage ("timestamp", host_id, memory_free, cpu_idel, cpu_kernel, disk_io,disk_available)VALUES ('2019-05-29 15:01:00.000', 2, 100000, 90, 4, 2, 3);
+INSERT INTO host_usage ("timestamp", host_id, memory_free, cpu_idel, cpu_kernel, disk_io,disk_available)VALUES ('2019-05-29 15:02:00.000', 2, 200000, 90, 4, 2, 3);
+INSERT INTO host_usage ("timestamp", host_id, memory_free, cpu_idel, cpu_kernel, disk_io,disk_available)VALUES ('2019-05-29 15:05:00.000', 2, 300000, 90, 4, 2, 3);
+INSERT INTO host_usage ("timestamp", host_id, memory_free, cpu_idel, cpu_kernel, disk_io,disk_available)VALUES ('2019-05-29 15:06:00.000', 2, 100000, 90, 4, 2, 3);
+INSERT INTO host_usage ("timestamp", host_id, memory_free, cpu_idel, cpu_kernel, disk_io,disk_available)VALUES ('2019-05-29 15:07:00.000', 2, 200000, 90, 4, 2, 3);
+INSERT INTO host_usage ("timestamp", host_id, memory_free, cpu_idel, cpu_kernel, disk_io,disk_available)VALUES ('2019-05-29 15:08:00.000', 2, 100000, 90, 4, 2, 3);
+INSERT INTO host_usage ("timestamp", host_id, memory_free, cpu_idel, cpu_kernel, disk_io,disk_available)VALUES ('2019-05-29 15:09:00.000', 2, 200000, 90, 4, 2, 3);
+
